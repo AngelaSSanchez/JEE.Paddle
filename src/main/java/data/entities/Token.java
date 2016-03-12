f482e86f3f99591e3765e468198a2dd3a2eadf65
AngelaSSanchez <angela.serranosanchez@gmail.com>
2016-03-12 17:36:27 +0100

    New attribute expiringTime in Token entity, new getter and setter
    methods for the new attribute and new boolean hasExpired function

--- src/main/java/data/entities/Token.java
+++ src/main/java/data/entities/Token.java
@@ -1,5 +1,6 @@
 package data.entities;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -12,12 +13,17 @@ import javax.persistence.ManyToOne;
 @Entity
 public class Token {
 
+	private static long ONEHOUR = 1000*60*60;
+	
     @Id
     @GeneratedValue
     private int id;
 
     @Column(unique = true, nullable = false)
     private String value;
+    
+    @Column(nullable = false)
+    private Calendar expiringTime;
 
     @ManyToOne
     @JoinColumn
@@ -31,6 +37,8 @@ public class Token {
         this.user = user;
         this.value = new Encrypt().encryptInBase64UrlSafe("" + user.getId() + user.getUsername() + Long.toString(new Date().getTime())
                 + user.getPassword());
+        this.expiringTime = Calendar.getInstance();
+        this.expiringTime.add(Calendar.HOUR_OF_DAY, 1);
     }
 
     public int getId() {
@@ -45,7 +53,19 @@ public class Token {
         return user;
     }
 
-    @Override
+    public Calendar getExpiringTime() {
+		return expiringTime;
+	}
+
+	public void setExpiringTime(Calendar expiringTime) {
+		this.expiringTime = expiringTime;
+	}
+	
+	public boolean hasExpired(){
+		return (Calendar.getInstance().getTimeInMillis() - this.expiringTime.getTimeInMillis() > ONEHOUR);
+	}
+
+	@Override
     public int hashCode() {
         return id;
     }
@@ -66,6 +86,6 @@ public class Token {
 
     @Override
     public String toString() {
-        return "Token [id=" + id + ", value=" + value + ", userId=" + user.getId() + "]";
+        return "Token [id=" + id + ", value=" + value + ", userId=" + user.getId() + ", expiringTime=" + expiringTime.getTime().toString() + "]";
     }
 }
