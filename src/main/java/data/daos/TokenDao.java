2d89d4ed3aa4e5bf9a956df15c4cdb419981ac4c
AngelaSSanchez <angela.serranosanchez@gmail.com>
2016-03-12 19:37:31 +0100

    Added method in TokenDao that filters the expired tokens

--- src/main/java/data/daos/TokenDao.java
+++ src/main/java/data/daos/TokenDao.java
@@ -1,5 +1,8 @@
 package data.daos;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import data.entities.Token;
@@ -8,4 +11,6 @@ import data.entities.User;
 public interface TokenDao extends JpaRepository<Token, Integer> {
 
     Token findByUser(User user);
+    
+    List<Token> findByExpiringTimeLessThan(Calendar time);
 }
