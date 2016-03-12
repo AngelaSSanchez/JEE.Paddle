138b9f46f6dfbece78e139a08634a4f399c2d46d
AngelaSSanchez <angela.serranosanchez@gmail.com>
2016-03-12 17:44:41 +0100

    Refactoring class to reuse testing code

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -5,41 +5,46 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import data.entities.Token;
 import data.entities.User;
 
 public class TokenTest {
+	
+	private User user;
 
-    @Test
-    public void testTokenUser() {
-        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
-        Token token = new Token(user);
-        //System.out.println(token.toString());
-        assertTrue(token.getValue().length() > 20);
-    }
-    
-    @Test
-    public void testTokenHasExpired() {
-        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
-        Token token = new Token(user);
-        Calendar time = Calendar.getInstance();
-        time.add(Calendar.HOUR_OF_DAY, -2);
-        token.setExpiringTime(time);
-        System.out.println("TOKEN "+token.toString()+" HORA ACTUAL: "+Calendar.getInstance().getTime().toString());
-        assertTrue(token.hasExpired());
-    }
-    
-    @Test
-    public void testTokenHasNotExpired() {
-        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
-        Token token = new Token(user);
-        Calendar time = Calendar.getInstance();
-        time.add(Calendar.HOUR_OF_DAY, 3);
-        token.setExpiringTime(time);
-        System.out.println("TOKEN "+token.toString()+" HORA ACTUAL: "+Calendar.getInstance().getTime().toString());
-        assertFalse(token.hasExpired());
-    }
+	private Token token;
+
+	private Calendar time;
+
+	@Before
+	public void testCreateTokenUser(){
+		this.user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+		this.token = new Token(user);
+		this.time = Calendar.getInstance();
+	}
+
+	@Test
+	public void testTokenUser() {
+	    assertTrue(this.token.getValue().length() > 20);
+	}
+
+	@Test
+	public void testTokenHasExpired() {
+		this.time.add(Calendar.HOUR_OF_DAY, -2);
+		this.token.setExpiringTime(this.time);
+		System.out.println("TOKEN " + this.token.toString() + " HORA ACTUAL: " + Calendar.getInstance().getTime().toString());
+		assertTrue(token.hasExpired());
+	}
+
+	@Test
+	public void testTokenHasNotExpired() {
+		this.time.add(Calendar.HOUR_OF_DAY, 3);
+		this.token.setExpiringTime(this.time);
+		System.out.println("TOKEN " + this.token.toString() + " HORA ACTUAL: " + Calendar.getInstance().getTime().toString());
+		assertFalse(token.hasExpired());
+	}
 
 }
