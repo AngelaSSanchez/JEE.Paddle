ed47d721cf6177b644d164d3e135efe06849c912
AngelaSSanchez <angela.serranosanchez@gmail.com>
2016-03-12 19:39:22 +0100

    Updated TokenDaoITest to test the method that filters the expired tokens

--- src/test/java/data/daos/TokenDaoITest.java
+++ src/test/java/data/daos/TokenDaoITest.java
@@ -3,6 +3,9 @@ package data.daos;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import java.util.Calendar;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +33,21 @@ public class TokenDaoITest {
         User user = (User) daosService.getMap().get("u4");
         assertEquals(token, tokenDao.findByUser(token.getUser()));
         assertNull(tokenDao.findByUser(user));
+    }
+    
+    @Test
+    public void testFindByExpiringTimeLessThan() {
+        Token token = (Token) daosService.getMap().get("tu1");
+        Calendar time = token.getExpiringTime();
+        time.add(Calendar.HOUR_OF_DAY, -2);
+        token.setExpiringTime(time);
+        tokenDao.save(token);
+        
+        List<Token> tokens = tokenDao.findByExpiringTimeLessThan(Calendar.getInstance());
+        for(Token t : tokens){
+        	tokenDao.findByUser(t.getUser());
+        }
+        assertEquals(token, tokenDao.findByExpiringTimeLessThan(Calendar.getInstance()).get(0));
     }
 
 }
