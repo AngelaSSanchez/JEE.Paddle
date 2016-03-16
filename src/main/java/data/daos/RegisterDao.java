b3df5e0810e2d01a3d4ecb23cebc9e0e86d15952
AngelaSSanchez <angela.serranosanchez@gmail.com>
2016-03-16 21:57:31 +0100

    Query in RegisterDao to know if a training is complete or not

--- src/main/java/data/daos/RegisterDao.java
+++ src/main/java/data/daos/RegisterDao.java
@@ -1,9 +1,12 @@
 package data.daos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import data.entities.Register;
 
 public interface RegisterDao extends JpaRepository<Register, Integer>{
-
+	
+	@Query(value = "select count(1) from register where training_id = ?", nativeQuery = true)
+	public int findTrainingIsComplete(int id);
 }
