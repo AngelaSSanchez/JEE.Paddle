ad703001929698a819a93010d27171c2a9dde6b3
AngelaSSanchez <angela.serranosanchez@gmail.com>
2016-03-25 21:18:26 +0100

    Refactoring of RegisterDao and TrainingDao

--- src/main/java/data/daos/RegisterDao.java
+++ src/main/java/data/daos/RegisterDao.java
@@ -1,12 +1,20 @@
 package data.daos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import data.entities.Register;
+import data.entities.Training;
+import data.entities.User;
 
-public interface RegisterDao extends JpaRepository<Register, Integer>{
+public interface RegisterDao extends JpaRepository<Register, Integer>, RegisterExtended{
 	
 	@Query(value = "select count(1) from register where training_id = ?", nativeQuery = true)
 	public int findTrainingIsComplete(int id);
+	
+	Register findByUserAndTraining(User user, Training training);
+	
+	List<Register> findByTraining(Training trainingd);
 }
--- src/main/java/data/daos/TrainingDao.java
+++ src/main/java/data/daos/TrainingDao.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import data.entities.Training;
 
-public interface TrainingDao extends JpaRepository<Training, Integer>{
-
+public interface TrainingDao extends JpaRepository<Training, Integer>, TrainingExtended{
+	
+	Training findById(int id);
+	
 }
